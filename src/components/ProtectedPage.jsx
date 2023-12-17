835e69f3f59f2e811fe457b2b4ed102489abe7fd
Anas Siddiqui <anassiddiqui0910@gmail.com>
2023-12-17 14:35:54 +0500

    Route Protection Added

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -1,9 +1,18 @@
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+
 function ProtectedPage() {
-  return (
-    <>
-      <h1>Welcome</h1>
-    </>
-  );
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isSignedIn) navigate("/sign-in");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return isSignedIn && <Outlet />;
 }
 
 export default ProtectedPage;
